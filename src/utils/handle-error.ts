8f82e85d6b70246868a140a8296516f209c14295
Yakubu Emmanuel <yakubuemmanuelsimpa@gmail.com>
2024-03-24 01:54:39 +0100

    fix on handle error file

--- src/utils/handle-error.ts
+++ src/utils/handle-error.ts
@@ -1,12 +1,12 @@
 import { toast } from "react-toastify";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const handleError = (error:any) => {
-  if (error.response) {
+export const handleError = (error: any) => {
+  if (error?.response) {
     // The request was made and the server responded with a status code
     // outside the range of 2xx
     toast.error(error.response.data.message);
-  } else if (error.request) {
+  } else if (error?.request) {
     // The request was made but no response was received
     toast.error("No response from server.");
   } else {
